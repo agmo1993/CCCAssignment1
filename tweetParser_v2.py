114340c7d55e6118047998e42e10be02cc85989e
agmo1993 <53358586+agmo1993@users.noreply.github.com>
2020-04-03 11:32:28 +1100

    updated with mpi
    program modified to run in 4 processes and tested on sbatch

--- tweetParser_v2.py
+++ tweetParser_v2.py
@@ -1,31 +1,68 @@
 import json
 import re
 
-tweet_list = []
+import numpy as np
+from mpi4py import MPI
 
+
+tweet_list = []
+dict_of_tweets = {}
+
+comm = MPI.COMM_WORLD
+
+rank, size = comm.Get_rank(), comm.Get_size()
 
 filehandle = open("smallTwitter.json", 'r',encoding='utf-8')
-while True:
-    # read a single line
-    line = filehandle.readline()
-    if not line:
-        break
-        
-    if "text" in line:
-        """
-        line_to_consider = line[:-1]
-        tweet_row = json.loads(line_to_consider)
-        tweet = tweet_row['doc']
-        ind_tweet_text = tweet['text']
-        """
-        tweets = re.findall('(?:\#+[\w_]+[\w\'_\-]*[\w_]+)',line)
-        tweet_list += tweets
+
+def tweetsToDict(tweetlist, tweetDict):
+    for i in tweetlist:
+        tweet = i.encode("UTF-8")
+        if tweet in tweetDict:
+            tweetDict[tweet] += 1
+        else:
+            tweetDict[tweet] = 1
+    return tweetDict
     
 
 
+while True:
+    comm.Barrier()
+    if rank == 0:
+        line = filehandle.readline()
+        if line:
+            comm.send(line, dest=1)
+        if not line:
+            comm.send("break", dest=1)
+            break
 
-tweet_list.sort()
+    elif rank == 1:
+        line = comm.recv(source=0)
+        if line == "break":
+            comm.send("break",dest=2)
+            break
+
+        if "text" in line:
+            """
+            line_to_consider = line[:-1]
+            tweet_row = json.loads(line_to_consider)
+            tweet = tweet_row['doc']
+            ind_tweet_text = tweet['text']
+            """
+            tweets = re.findall('(?:\#+[\w_]+[\w\'_\-]*[\w_]+)',line)
+            comm.send(tweets, dest=2)
+        else:
+            comm.send("None",dest=2)
+        
+
+        
+    elif rank == 2:
+        tweets = comm.recv(source=1)
+        dict_of_tweets = tweetsToDict(tweets, dict_of_tweets)
+        if tweets == "break":
+            dict_sorted =  sorted(dict_of_tweets.items(),key=lambda x: x[1])
+            print(dict_sorted[-10:])
+            break
 
 
-print(tweet_list)
-print(len(tweet_list))
+
+    
