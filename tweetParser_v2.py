bd7a23a9a260967bb0b6c5108157fe269cb206dd
agmo1993 <53358586+agmo1993@users.noreply.github.com>
2020-04-04 21:32:29 +1100

    barrier repositioned

--- tweetParser_v2.py
+++ tweetParser_v2.py
@@ -7,6 +7,7 @@ from mpi4py import MPI
 
 tweet_list = []
 dict_of_tweets = {}
+dict_of_lang = {}
 
 comm = MPI.COMM_WORLD
 
@@ -22,17 +23,20 @@ def tweetsToDict(tweetlist, tweetDict):
         else:
             tweetDict[tweet] = 1
     return tweetDict
-    
 
+
+comm.Barrier()
 
 while True:
-    comm.Barrier()
+    
     if rank == 0:
         line = filehandle.readline()
         if line:
             comm.send(line, dest=1)
+            comm.send(line, dest=3)
         if not line:
             comm.send("break", dest=1)
+            comm.send("break", dest=3)
             break
 
     elif rank == 1:
@@ -62,6 +66,31 @@ while True:
             dict_sorted =  sorted(dict_of_tweets.items(),key=lambda x: x[1])
             print(dict_sorted[-10:])
             break
+
+    elif rank == 3:
+        line = comm.recv(source=0)
+        if line == "break":
+            comm.send("break",dest=4)
+            break
+
+        if 'lang' in line:
+            line_dict = json.loads(line.rstrip(",\n"))
+            language = line_dict['doc']['lang']
+            comm.send(language,dest=4)
+        else:
+            comm.send("None",dest=4)
+    
+    elif rank == 4:
+        language = comm.recv(source=3)
+        if language in dict_of_lang:
+            dict_of_lang[language] += 1
+        else:
+            dict_of_lang[language] = 1
+        if language == "break":
+            lang_sorted = sorted(dict_of_lang.items(),key=lambda x: x[1])
+            print(lang_sorted[-10:])
+            break
+
 
 
 
