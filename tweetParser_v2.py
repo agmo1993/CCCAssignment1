8dd9dc4debfa745fc660dd063ba7d315d9493403
agmo1993 <53358586+agmo1993@users.noreply.github.com>
2020-04-06 21:36:02 +1000

    with comments

--- tweetParser_v2.py
+++ tweetParser_v2.py
@@ -15,6 +15,9 @@ rank, size = comm.Get_rank(), comm.Get_size()
 
 filehandle = open("smallTwitter.json", 'r',encoding='utf-8')
 
+
+"""method updates the dictionary kept locally keeping the count of the respective tweets"""
+
 def tweetsToDict(tweetlist, tweetDict):
     for i in tweetlist:
         tweet = i.encode("UTF-8")
@@ -29,6 +32,9 @@ comm.Barrier()
 
 
 while True:
+
+    #If the software is running with a single process, the size is 1 and hence the block below is called
+
     if size == 1:
         line = filehandle.readline()
         if not line:
@@ -39,12 +45,6 @@ while True:
             break
         tweets = []
         if "text" in line:
-            """
-            line_to_consider = line[:-1]
-            tweet_row = json.loads(line_to_consider)
-            tweet = tweet_row['doc']
-            ind_tweet_text = tweet['text']
-            """
             tweets = re.findall('(?:\#+[\w_]+[\w\'_\-]*[\w_]+)',line)
             dict_of_tweets = tweetsToDict(tweets, dict_of_tweets)
 
@@ -57,36 +57,40 @@ while True:
                 dict_of_lang[language] = 1
 
 
+    #If the software is run on multiple processes the following block is called 
+
     else:
+
+        """The first process reads a line and send the line to the second process and fourth processes"""
+
         if rank == 0:
             line = filehandle.readline()
             if line:
                 comm.send(line, dest=1)
                 comm.send(line, dest=3)
-            if not line:
-                comm.send("break", dest=1)
-                comm.send("break", dest=3)
+            if not line: #if no line is left to read, a break command is sent to processes 2 and 4
+                comm.send("break", dest=1) #check for tweets
+                comm.send("break", dest=3) #check for language handle
                 break
 
+        
+        
         elif rank == 1:
             line = comm.recv(source=0)
             if line == "break":
                 comm.send("break",dest=2)
                 break
 
+            """ check the line for the presence of the text, then uses regex to find the tweets"""
+
             if "text" in line:
-                """
-                line_to_consider = line[:-1]
-                tweet_row = json.loads(line_to_consider)
-                tweet = tweet_row['doc']
-                ind_tweet_text = tweet['text']
-                """
                 tweets = re.findall('(?:\#+[\w_]+[\w\'_\-]*[\w_]+)',line)
                 comm.send(tweets, dest=2)
             else:
                 comm.send("None",dest=2)
             
 
+        """ collect tweets and language, updates local dictionary and the prints the output once the file has been completely read """
             
         elif rank == 2:
             tweets = comm.recv(source=1)
@@ -105,6 +109,8 @@ while True:
                 print(lang_sorted[-10:])
                 break
 
+
+        """ checks the language and sends it to the third process  """
 
         elif rank == 3:
             line = comm.recv(source=0)
